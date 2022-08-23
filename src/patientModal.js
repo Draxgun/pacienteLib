dce780895c9c8140164cbf82606d8f58577b7e94
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-23 13:32:13 -0600

    Changes to update csv files

--- src/patientModal.js
+++ src/patientModal.js
@@ -8,17 +8,48 @@ import {createModalStructure} from './modalCreators'
 let addPatientModalContent = () => {
 
     /* Main container for the modal */
-    let mainContainer = createDOMContainer('addPatient');
+    let mainForm = createDOMElement('form','patientForm','','')
 
-    /*Main Design*/
-    let testModal = createDOMElement('div','prueba','jamon')
-    mainContainer.appendChild(testModal)
+    /* Patient form sides */
+    let rightSide = createDOMContainer('patientFormRight')
+    let leftSide = createDOMContainer('patientFormLeft')
 
-    let addButton = document.getElementById('addPatientButton')
-
-    createModalStructure(mainContainer,'modalMain','Jamon',addButton)
-
+    /*Main Design Right Side*/
+    let nameLabel = createDOMElement('label','formTitle','Nombre')
+    nameLabel.setAttribute('for','fname')
     
+    let nameInput = createDOMElement('input','formTextInput','','fname')
+    nameInput.type = 'text'
+
+    rightSide.appendChild(nameLabel);
+    rightSide.appendChild(nameInput);
+
+    /*General*/
+    let generalLabel = createDOMElement('label','formTitle','Diagnostico General')
+    generalLabel.setAttribute('for','fgeneral')
+    
+    let generalInput = createDOMElement('select','selectInput','','fgeneral')
+
+    rightSide.appendChild(generalLabel);
+    rightSide.appendChild(generalInput);
+
+    /* Especifico */
+    let specificLabel = createDOMElement('label','formTitle','Diagnostico Especifico')
+    specificLabel.setAttribute('for','fspecific')
+    
+    let specificInput = createDOMElement('select','selectInput','','fspecific')
+
+    rightSide.appendChild(specificLabel);
+    rightSide.appendChild(specificInput);
+
+    /* Tratamientos*/
+    
+
+
+    mainForm.appendChild(rightSide);
+
+    return mainForm
+ 
 }
 
 export{
