6bf9da652a2d28cbbade3b967db5cdf1951a3547
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-24 13:48:12 -0600

    Modal structure and layout done need to change css

--- src/patientModal.js
+++ src/patientModal.js
@@ -26,6 +26,9 @@ let addPatientModalContent = () => {
     let rightSide = createDOMContainer('patientFormRight')
     let leftSide = createDOMContainer('patientFormLeft')
 
+    let topSide = createDOMContainer('patientFormTop')
+    let bottomSide = createDOMContainer('patientFormBottom')
+
     /*Main Design Right Side*/
     let nameLabel = createDOMElement('label','formTitle','Nombre')
     nameLabel.setAttribute('for','fname')
@@ -35,6 +38,7 @@ let addPatientModalContent = () => {
 
     rightSide.appendChild(nameLabel);
     rightSide.appendChild(nameInput);
+
 
     /*General*/
     let generalLabel = createDOMElement('label','formTitle','Diagnostico General')
@@ -46,9 +50,6 @@ let addPatientModalContent = () => {
     for(let i = 0; i < general.length; i++){
         generalInput.options.add(new Option(general[i], general[i]));
     }   
-
-
-
 
     rightSide.appendChild(generalLabel);
     rightSide.appendChild(generalInput);
@@ -81,21 +82,138 @@ let addPatientModalContent = () => {
 
     })
 
-
-
     rightSide.appendChild(specificLabel);
     rightSide.appendChild(specificInput);
 
     /* Tratamientos*/
+    let tratamientosLabel = createDOMElement('label','formTitle','Tratamiento')
+    tratamientosLabel.setAttribute('for','ftratamiento')
     
+    let tratamientosInput = createDOMElement('select','selectInput','','ftratamiento')
+    
+    for(let i = 0; i < tratamientos.length; i++){
+        tratamientosInput.options.add(new Option(tratamientos[i], tratamientos[i]));
+    } 
+
+    rightSide.appendChild(tratamientosLabel);
+    rightSide.appendChild(tratamientosInput);
+
+    /* Alergias Container */
+    let alergiasContainer = createDOMContainer('alergiasContainer')
+
+    /*Alergias*/
+    let alergiasLabel = createDOMElement('label','formTitle','Alergias')
+    alergiasContainer.appendChild(alergiasLabel)
+
+    for (let i=0; i<alergias.length;i++){
+        console.log(alergias[i])
+        let currentAlergyInput = createDOMElement('input','alergyCheckbox','',`alergy${alergias[i]}`)
+        currentAlergyInput.type = 'checkbox'
+        currentAlergyInput.setAttribute('name',alergias[i])
+        currentAlergyInput.setAttribute('value',alergias[i])
+
+        let currentAlergyLabel = createDOMElement('label','checboxLabel',`${alergias[i]}`)
 
 
-    mainForm.appendChild(rightSide);
+        let container = createDOMContainer('checkbox')
+        container.appendChild(currentAlergyInput)
+        container.appendChild(currentAlergyLabel)
+
+        alergiasContainer.appendChild(container)
+    }
+
+    bottomSide.appendChild(alergiasContainer)
+
+    /* enfermedades Container */
+    let enfermedadesContainer = createDOMContainer('enfermedadesContainer')
+
+    /*enfermedades*/
+    let enfermedadesLabel = createDOMElement('label','formTitle','enfermedades')
+    enfermedadesContainer.appendChild(enfermedadesLabel)
+
+    for (let i=0; i<enfermedades.length;i++){
+        console.log(enfermedades[i])
+        let currentAlergyInput = createDOMElement('input','alergyCheckbox','',`alergy${enfermedades[i]}`)
+        currentAlergyInput.type = 'checkbox'
+        currentAlergyInput.setAttribute('name',enfermedades[i])
+        currentAlergyInput.setAttribute('value',enfermedades[i])
+
+        let currentAlergyLabel = createDOMElement('label','checboxLabel',`${enfermedades[i]}`)
+
+
+
+
+        let container = createDOMContainer('checkbox')
+        container.appendChild(currentAlergyInput)
+        container.appendChild(currentAlergyLabel)
+
+        enfermedadesContainer.appendChild(container)
+    }
+
+    bottomSide.appendChild(enfermedadesContainer)
+
+    /* habitos Container */
+    let habitosContainer = createDOMContainer('habitosContainer')
+
+    /*habitos*/
+    let habitosLabel = createDOMElement('label','formTitle','habitos')
+    habitosContainer.appendChild(habitosLabel)
+
+    for (let i=0; i<habitos.length;i++){
+        console.log(habitos[i])
+        let currentAlergyInput = createDOMElement('input','alergyCheckbox','',`alergy${habitos[i]}`)
+        currentAlergyInput.type = 'checkbox'
+        currentAlergyInput.setAttribute('name',habitos[i])
+        currentAlergyInput.setAttribute('value',habitos[i])
+
+        let currentAlergyLabel = createDOMElement('label','checboxLabel',`${habitos[i]}`)
+
+        let container = createDOMContainer('checkbox')
+        container.appendChild(currentAlergyInput)
+        container.appendChild(currentAlergyLabel)
+
+        habitosContainer.appendChild(container)
+    }
+    
+    bottomSide.appendChild(habitosContainer)
+
+    /* Left Side */
+
+    /*AGE*/
+    let edadLabel = createDOMElement('label','formTitle','Edad')
+    edadLabel.setAttribute('for','fedad')
+
+    let edadInput = createDOMElement('input','ageInput','','fedad')
+    edadInput.type = 'number'
+
+    leftSide.appendChild(edadLabel)
+    leftSide.appendChild(edadInput)
+
+    /* Sexo */
+    let sexoLabel = createDOMElement('label','formTitle','Sexo')
+    sexoLabel.setAttribute('for','fsexo')
+
+    let sexoInput = createDOMElement('select','sexInput','','fsexo')
+    let sexes = ['Masculino','Femenino','Otro']
+
+    for(let i = 0; i < sexes.length; i++){
+        sexoInput.options.add(new Option(sexes[i], sexes[i]));
+    } 
+
+    leftSide.appendChild(sexoLabel)
+    leftSide.appendChild(sexoInput)
+
+
+    topSide.appendChild(rightSide);
+    topSide.appendChild(leftSide);
+    mainForm.appendChild(topSide)
+    mainForm.appendChild(bottomSide);
+
 
 
     let addButton = document.getElementById('addPatientButton')
 
-    createModalStructure(mainForm,'modal','addPatientModal',addButton)
+    createModalStructure(mainForm,'modal','Agregar Paciente',addButton)
     return mainForm
  
 }
